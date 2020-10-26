eab79c393a406297a41b0b6156a32c499ce44e5b
anborg <anborg@gm.com>
2020-10-26 14:59:58 -0400

    template

--- run.bat
+++ run.bat
@@ -1,2 +1,10 @@
-cp testfiles/cayinput566000.csv input/AP566000
-go-mail.exe
+::cp testfiles/cayinput566000.csv input/AP566000
+::go-mail.exe
+
+cp \\markham.ca\apps\eft\test_files\cayinput566000.csv \\markham.ca\apps\eft\input\AP566000
+
+:: Run with uid/pwd
+schtasks /run /tn "\mkm\eft" /s wdnvmhapp98 /u s_application /p app##svc@
+
+:: Run without uid
+::schtasks /run /tn "\mkm\eft" /s wdnvmhapp98
