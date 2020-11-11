b0089774e54e9d4b2b2be8d38559204f89518c0a
anborg <dev@gmail.com>
2020-11-11 09:58:28 -0500

    comments

--- util.go
+++ util.go
@@ -18,7 +18,7 @@ func substr(input string, start int, length int) string {
 	return string(asRunes[start : start+length])
 }
 
-func main() {
+func main2() {
 	var x string = ` (I)8751                       PB20034 EST2 ASPHALT            31/07/20 $469974.60 001000006
 		(I)8753                       PB20034 EST3 ASPHALT            15/08/20 $583537.82 001000006
 		(I)8682                       PB20034 EST20101 ASPHALT        30/06/20$1353810.33 001000006
