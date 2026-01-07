5e3cdaed53922fe5d89eb107f30210b5f65f0849
noname <noname@mail.com>
2026-01-06 21:29:48 -0500

    upgrade packages to latest version

--- cmd/go-mail/main.go
+++ cmd/go-mail/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"muni/go-mail/internal/config"
 	"muni/go-mail/internal/processor"
@@ -38,6 +41,10 @@ func main() {
 
 	//config log //TODO perhaps long to system.out sstem.error and then use infra to process log location/output?
 	logconf := conf.AppConfig.LumberjackLogConfig
+	
+	// Check if log file needs rotation (older than 1 month or > 10MB)
+	rotateLogIfNeeded(logconf.Filename)
+
 	log.SetOutput(&lumberjack.Logger{Filename: logconf.Filename, MaxSize: logconf.MaxSize, MaxBackups: logconf.MaxBackups, MaxAge: logconf.MaxAge, Compress: logconf.Compress})
 	fileProcessorConf := conf.FileProcessorConfig
 	if err := processor.EnsureMandatoryDirsExist(fileProcessorConf.InputDir, fileProcessorConf.DoneDir, fileProcessorConf.ErrorDir); err != nil {
@@ -63,3 +70,24 @@ func main() {
 	}
 
 } //main
+
+func rotateLogIfNeeded(filename string) {
+	if info, err := os.Stat(filename); err == nil {
+		// 1 month = 30 days roughly
+		isOld := time.Since(info.ModTime()) > 30*24*time.Hour
+		isLarge := info.Size() > 2*1024*1024 // 2MB
+
+		if isOld || isLarge {
+			ext := filepath.Ext(filename)
+			name := strings.TrimSuffix(filename, ext)
+			backupName := fmt.Sprintf("%s-%s%s", name, time.Now().Format("20060102-150405"), ext)
+
+			if err := os.Rename(filename, backupName); err != nil {
+				log.Printf("Failed to rotate existing log file: %v", err)
+			} else {
+				log.Printf("Rotated existing log file to: %s", backupName)
+			}
+		}
+	}
+}
+
