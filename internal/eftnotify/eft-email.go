19bffce916a94c63b0206ac5f8b747ade5edd6ce
noname <noname@mail.com>
2026-01-09 15:31:17 -0500

    refactor

--- internal/eftnotify/eft-email.go
+++ internal/eftnotify/eft-email.go
@@ -4,7 +4,8 @@ import (
     "crypto/tls"
     "log"
     "time"
-
+"bytes"
+	"muni/go-mail/internal/utils"
 	"muni/go-mail/internal/config"
 	gomail "gopkg.in/mail.v2"
 )
@@ -23,7 +24,7 @@ func BatchSendMail(conf config.MailServerConfig, myarray EftInfos) error {
         message.Reset()
         var eftinfo EftInfo = myarray.EftInfos[i]
         eftinfo.TodayDate = time.Now().Format("2006-01-02 15:04:05 Monday")
-        bytesHtml, err := GenerateEmailBody(eftinfo)
+        bytesHtml, err := generateEmailBody(eftinfo)
         if err != nil {
             return (err)
         }
@@ -40,4 +41,11 @@ func BatchSendMail(conf config.MailServerConfig, myarray EftInfos) error {
     }
 
     return nil
+}
+
+// GenerateEmailBody apply info on Template to create html
+func generateEmailBody(eftinfo EftInfo) (bytes.Buffer, error) {
+	eftTemplatePath := "templates/DEFAULT.gohtml"
+	// 	eftTemplatePath := "templates/SAMPLE_TEMPLATE.txt" //for testing
+	return utils.RenderTemplate(eftTemplatePath, eftinfo)
 }
